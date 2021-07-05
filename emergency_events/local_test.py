9e9e48d46f2fde997262236b236b2e05344cf192
Martin L. Brink <marty@mbrink.org>
2021-07-05 11:24:12 -0400

    Fixed local test to handle contents of test directory

--- emergency_events/local_test.py
+++ emergency_events/local_test.py
@@ -1,26 +1,22 @@
-import json
 import boto3
+from os import listdir
+from os.path import isfile, join
 from lambda_function import *
 
 
+# When not running in a lambda context, the region must be specified
 def get_db_connection():
     return boto3.resource('dynamodb', region_name='us-east-1')
 
 
 if __name__ == "__main__":
     db = get_db_connection()
+    working_directory = "../test_data"
+    process_files = [d for d in listdir(working_directory) if isfile(join(working_directory, d))]
 
-    f = open('../test_data/F01705150050.json',)
+    for data_source_file in process_files:
+        f = open(join(working_directory, data_source_file))
+        rpost = json.load(f)
+        write_event(db=db, in_event=rpost)
+        f.close()
 
-    rpost = json.load(f)
-    write_event(db=db,in_event=rpost)
-
-    f.close()
-
-
-    f = open('../test_data/F01705150090.json',)
-
-    rpost = json.load(f)
-    write_event(db=db,in_event=rpost)
-
-    f.close()
